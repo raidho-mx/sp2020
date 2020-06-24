dec20f956ce7b104957a66956ab61574c211920e
Linda Castillo <lindacastillor@gmail.com>
2020-06-24 16:57:30 -0500

    Agregar pantalla buró de entidades financieras

---
+++ buro-entidades-financieras.php
@@ -0,0 +1,182 @@
+
+<?php include('header.php'); ?>
+
+<!-- BLOQUE HERO IMAGE -->
+<section>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 hero-img pt-100 pb-100" style="background-image: url('img/bg-home1.png');"></div>
+		</div>
+		<div class="d-lg-none d-inline-block m-auto pb-4 pt-4 text-center w-100">
+			<button class="has-btn btn-primary bg-blue m-auto" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+				Ver más sobre Regulación
+			</button>
+			<div class="container collapse" id="collapseExample">
+				<div class="row pt-3 pb-5" id="numbers2">
+					<div class="text-center col-12 border-right-light-gray pt-3 pb-3 border-bottom-light-gray">
+						<p class="txt16 is-bold c-dark-blue"><a href="terminos-condiciones-servicios-electronicos.php">TÉRMINOS Y CONDICIONES DE SERVICIOS ELECTRÓNICOS</a></p>
+					</div>
+					<div class="text-center col-12 border-right-light-gray pt-3 pb-3 border-bottom-light-gray">
+						<p class="txt16 is-bold c-dark-blue"><a href="terminos-condiciones-portal.php">TÉRMINOS Y CONDICIONES DEL PORTAL</a></p>
+					</div>
+					<div class="text-center col-12 border-right-light-gray pt-3 pb-3 border-bottom-light-gray">
+						<p class="txt16 is-bold c-dark-blue"><a href="terminos-condiciones-referidos.php">TÉRMINOS Y CONDICIONES PROGRAMA REFERIDOS</a></p>
+					</div>
+					<div class="text-center col-12 border-right-light-gray pt-3 pb-3 border-bottom-light-gray">
+						<p class="txt16 is-bold c-dark-blue"><a href="terminos-condiciones-super-referidos.php">TÉRMINOS Y CONDICIONES PROGRAMA SUPER REFERIDOS</a></p>
+					</div>
+					<div class="text-center col-12 pt-3 pb-3">
+						<p class="txt16 is-bold c-dark-blue"><a href="politica-privacidad.php">AVISO DE PRIVACIDAD</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- BLOQUE FILTRAR ENTRADAS -->
+<section class="pb-100 pt-100 fade-in delay1">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 c-dark-blue txt16">
+				<p class="is-bold txt24 text-center text-uppercase mb-md-4 mb-3">Buró de Entidades Financieras</p>
+				<p class="mb-md-5 mb-3">El BEF es una herramienta de consulta en la que podrás conocer el comportamiento de las instituciones y los productos que ofrecen.</p>
+				<p class="is-bold mb-md-5 mb-3">¿Qué es el Buró de Entidades Financieras?</p>
+				<p class="mb-md-5 mb-3">Es una herramienta de consulta y difusión con la que podrá conocer los productos que ofrecen las entidades financieras, sus comisiones y tasas, las reclamaciones de los usuarios, las prácticas no sanas en que incurren, las sanciones administrativas que les han impuesto, las cláusulas abusivas de sus contratos y otra información que resulte relevante para informarle sobre su desempeño.</p>
+				<p class="mb-md-5 mb-3">Con el Buró de Entidades Financieras, se logrará saber quién es quién en bancos, seguros, sociedades financieras de objeto múltiple, cajas de ahorro, afores, entre otras entidades. Con ello, podrá comparar y evaluar a las entidades financieras, sus productos y servicios y tendrá mayores elementos para elegir lo que más le convenga.</p>
+				<p class="mb-md-5 mb-3">Esta información le será útil para elegir un producto financiero y también para conocer y usar mejor los que ya tiene. Este Buró de Entidades Financieras es una herramienta que puede contribuir al crecimiento económico del país, al promover la competencia entre las instituciones financieras; que impulsará la transparencia al revelar información a los usuarios sobre el desempeño de éstas y los productos que ofrecen y que va a facilitar  un manejo responsable de los productos y servicios financieros al conocer a detalle sus características.</p>
+				<p class="mb-md-5 mb-3">Lo anterior, podrá derivar en un mayor bienestar social, porque al conjuntar en un solo espacio tan diversa información del sistema financiero, usted como usuario tendrá más elementos para optimizar su presupuesto, para mejorar sus finanzas personales, para utilizar correctamente los créditos que fortalecerán su economía y obtener los seguros que la protejan, entre otros aspectos.</p>
+				<p class="mb-md-5 mb-3">La información que consta en Buró de Entidades Financieras de CREDICLUB es la siguiente:</p>
+				<div class="overflow-x-scroll mb-3">
+					<table class="has-table c-dark-blue mt-md-4 mt-3 mb-md-4 mb-3 txt12" style="width: 1700px;">
+						<thead>
+							<tr class="text-center" style="background-color:#002C66; color: white;">
+								<td>INSTITUCIONES</li>
+								<td>
+									<p>OFERTA DE PRODUCTOS</p>
+								</td>
+								<td>
+									<p>CONSULTAS, RECLAMACIONES Y CONTROVERSIAS</p>
+									<div class="d-flex justify-content-between">
+										<p>Total de Reclamaciones</p>
+										<p>Indice de Reclamación por cada 10 mil clientes</p>
+										<p>Indice de desempeño de atención a usuarios <br>(IDATU) <br>0-10</p>
+									</div>
+								</td>
+								<td>
+									<p>SANCIONES</p>
+									<div class="d-flex justify-content-between">
+										<p>Total de Sanciones</p>
+										<p>Monto Total de Sanción <br>( $ )</p>
+									</div>
+								</td>
+								<td>
+									<p>CLAUSULAS ABUSIVAS PARTICULARES (EN PROCESO DE ELIMINACIÓN)</p>
+								</td>
+								<td>
+									<p>CUMPLIMIENTO A LOS REGISTROS DE CONDUSEF</p>
+									<div class="sub-columnas">
+										<p>Calificación <br> 0 - 10</p>
+									</div>
+								</td>
+								<td>
+									<p>PROGRAMAS DE EDUCACIÓN FINANCIERA</p>
+								</td>
+								<td>
+									<p>EVALUACIÓN DE CONDUSEF POR PRODUCTO</p>
+									<div class="sub-columnas">
+										<p>Calificación <br>0 - 10</p>
+									</div>
+								</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><p>TOTALES DEL SECTOR</p></td>
+								<td></td>
+								<td>
+									<div class="d-flex justify-content-between">
+										<p>7,938</p>
+										<p>10.3</p>
+										<p>8.19</p>
+									</div>
+								</td>
+								<td>
+									<div class="d-flex justify-content-between">
+										<p>122</p>
+										<p>12,330,194</p>
+									</div>
+								</td>
+								<td class="text-center">
+									<p>0</p>
+								</td>
+								<td class="text-center">
+									<div class="sub-columnas">
+										<p>9.6</p>
+									</div>
+								</td>
+								<td>
+									<p></p>
+								</td>
+								<td>
+									<div class="sub-columnas">
+										<p></p>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<p>Crediclub, S.A. de C.V., S.F.P.</p>
+								</td>
+								<td class="text-center">
+									<u><a href="http://ifit.condusef.gob.mx/ifit/ftb_validaciones.php?idi=13939">Ficha</a></u>
+								</td>
+								<td>
+									<div class="d-flex justify-content-between">
+										<p>32</p>
+										<p>0.8</p>
+										<p>9.97</p>
+									</div>
+								</td>
+								<td>
+									<div class="d-flex justify-content-between">
+										<p>0</p>
+										<p>-</p>
+									</div>
+								</td>
+								<td class="text-center">
+									<p>S/I</p>
+								</td>
+								<td class="text-center">
+									<div class="sub-columnas">
+										<p>10.0</p>
+									</div>
+								</td>
+								<td class="text-center">
+									<p>X</p>
+								</td>
+								<td class="text-center">
+									<div class="sub-columnas">
+										<u><a href="https://www.buro.gob.mx/general_gob.php?id_sector=27&amp;id_periodo=23">Ver</a></u>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<p class="txt10 mb-md-5 mb-3">
+				(1) Corresponde a la suma de reclamaciones presentadas en las Instituciones Financieras y en CONDUSEF <br>
+				NA: No aplica en aquellos casos en que la información de los reportes regulatorios de los que se toma la información no sea congruente.<br>
+				(2) Índice de Reclamación por cada 10,000 clientes.- Es el Total de Reclamaciones en la Condusef y en la Institución Financiera dividido entre el número de clientes de la institución financiera por 10,000<br>
+				NA: Para aquellas instituciones que no registraron controversias concluidas en el periodo no se genera Índice de Desempeño de Atención a Usuarios (IDATU). En el caso de la Evaluación de Condusef no cuenta con producto evaluados.
+				</p>
+				<p class="semi-subtitle mb-2"> Consulta la información disponible en el buró de entidades financieras de Crediclub SA de CV SFP en el siguiente  <a href="https://www.buro.gob.mx/general_gob.php?id_sector=27&amp;id_periodo=25" target="_blank">link</a>. </p>
+				<p class="semi-subtitle mb-2">La información aquí proporcionada corresponde únicamente a la de CREDICLUB y que para conocer la información de todo el sector de Sociedades Financieras Populares se podrá acceder al sitio <a href="http://www.buro.gob.mx" target="_blank"> http://www.buro.gob.mx.</a> </p>
+				<p class="semi-subtitle mb-2">El Buró de Entidades Financieras contiene información de CREDICLUB, S.A. DE C.V., SFP, sobre nuestro desempeño frente a los Usuarios, por la prestación de productos y servicios. Te invitamos a consultarlo en la página <a href="http://www.buro.gob.mx" target="_blank">http://www.buro.gob.mx</a> o en nuestra página de internet <a href="http://www.crediclub.com" target="_blank">www.crediclub.com.</a></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
